445fbabe9ad12b2d16cbc17249b00ecf95d2cb9e
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-03-04 17:22:59 +0530

    asdrfgr

---
+++ react-bootstrap/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Accordian from './components/Accordian'
+import Modals from './components/Modal'
+import './App.css'
+import Navr from './components/Nav'
+function App() {
+
+
+  return (
+    <>
+       <Navr/>
+      <Accordian/>
+      <Modals/>
+
+    </>
+  )
+}
+
+export default App
